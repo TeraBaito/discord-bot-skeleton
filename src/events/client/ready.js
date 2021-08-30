22f45de2a85f03fa12b4798fdb3093a2ea158cc9
TeraBaito <lrobles2670@gmail.com>
2021-08-30 14:57:21 -0600

    1.0.1 Fix JSDoc mistype

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -6,7 +6,7 @@ const chalk = require('chalk');
  * Triggers once the bot loads all the other events and goes online.
  * Useful to show ready messages and do/set things at startup.
  * 
- * @param {Client} bot 
+ * @param {Bot} bot 
  */
 module.exports = bot => {
     console.info(`${chalk.green('[Info]')} - ${bot.user.username} online!`);
